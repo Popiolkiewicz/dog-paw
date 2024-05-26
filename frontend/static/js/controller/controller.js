30b4795e9e1514546f577a506a72c894fdd820fd
hpopiolkiewicz <h.popiolkiewicz@gmail.com>
2024-05-26 08:03:04 +0200

    Generated Quarkus backend using mvn

--- frontend/static/js/controller/controller.js
+++ frontend/static/js/controller/controller.js
@@ -1,5 +1,6 @@
 export default class Controller {
-    host = "http://127.0.0.1:3333";
+    // host = "http://127.0.0.1:3333"; //mock backend
+    host = "http://localhost:8080"; //quarkus main backend
     apiUri = "/api";
     apiUrl = this.host + this.apiUri;
 }
